a37f11135c6d8370e893185939866e58c4718901
Vitus <tunv.b22cn748@stu.ptit.edu.vn>
2025-03-09 21:06:32 +0700

    Authetication Setup

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,16 @@
 import { Button } from "./components/ui/button"
-
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-600"> Hello world! </h1>
       <Button variant={"default"}> Hi </Button>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   )
 }
