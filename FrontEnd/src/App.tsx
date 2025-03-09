8be42e2ef059034dcf3726150936e20f892f5171
Vitus <tunv.b22cn748@stu.ptit.edu.vn>
2025-03-09 20:58:21 +0700

    FrontEnd Setup

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600"> Hello world! </h1>
+      <Button variant={"default"}> Hi </Button>
+    </>
+  )
+}
+
+export default App
