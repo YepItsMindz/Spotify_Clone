1049bf4b2e148d78223e7799ec97e74925cb17fa
Vitus <tunv.b22cn748@stu.ptit.edu.vn>
2025-03-09 23:46:32 +0700

    Admin Routes & Controllers

--- BackEnd/src/controller/auth.controller.js
+++ BackEnd/src/controller/auth.controller.js
@@ -1,7 +1,7 @@
 
-import User from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
-export const authCallback = async (req, res) => {
+export const authCallback = async (req, res, next) => {
   try {
     const {id, firstName, lastName, imageUrl} = req.body;
 
@@ -21,6 +21,6 @@ export const authCallback = async (req, res) => {
 
   } catch (error) {
     console.log("Error in auth callback", error);
-    return res.status(500).json({ message:"Internal server error", error });
+    next(error);
   }
 };
--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 import { connectDB } from './lib/db.js';
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
+import path from 'path';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import adminRoutes from './routes/admin.route.js';
@@ -13,11 +15,20 @@ import statRoutes from './routes/stat.route.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT;
 
-app.use(express.json()); // to parse request body
 
+app.use(express.json()); // to parse request body
 app.use(clerkMiddleware());  
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : path.join(__dirname, 'tmp'),
+  createParentPath: true,
+  limits: {
+    fileSize: 10 * 1024 * 1024 //10MB max file(s) size
+  },
+}));
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
@@ -26,7 +37,10 @@ app.use("/api/songs", songRoutes)
 app.use("/api/albums", albumRoutes)
 app.use("/api/stats", statRoutes)
 
-
+//error handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === 'production' ? 'Internal server error' : err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
